7a55a28979e9492653c2993b6472cff4f22b3cee
Nosdi67 <rshakhmur@gmail.com>
2024-02-20 16:33:17 +0100

    La c'est bon,

--- php_exo_4.php
+++ php_exo_4.php
@@ -3,13 +3,14 @@
 <p>Ecrire un algorithme permettant de savoir si une phrase est palindrome.</p>
 
 <?php
-$phrase_2 = "Engage le jeu que je le gagne";
+$phrase_1 = "Engage le jeu que je le gagne";
+$phrase_2 = $phrase_1 ;
 $phrase_2 = strtolower($phrase_2);
 $phrase_2 = strrev($phrase_2);
 $phrase_2 = str_replace(' ','',$phrase_2);
 
 if($phrase_2 == strrev($phrase_2)){
-    echo "La phrase « Engage le jeu que je le gagne » est palindrome";
+    echo "La phrase « $phrase_1 » est palindrome";
 } else { 
    echo "La phrase saisie n'est pas palindrome";
 }
