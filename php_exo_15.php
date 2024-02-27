549780fc7efb1879329df95cebfd0df0572b6d1b
Nosdi67 <rshakhmur@gmail.com>
2024-02-27 14:19:21 +0100

    Exo 15

--- php_exo_15.php
+++ php_exo_15.php
@@ -1,8 +1,8 @@
 <h1>Exo 15</h1>
 
 <p>Créer une classe Personne (nom, prénom et date de naissance).
-Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.
-$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;
+Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.<br>
+$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;<br>
 $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
 
 <?php
@@ -10,26 +10,42 @@ $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
 class personne{
     private $_nom;
     private $_prenom;
-    private $_date_de_naissance;
+    private $_dateDeNaissance;
+ 
 
-    public function__construct($_nom,$_prenom,$_date_de_naissance){
+    public function __construct($_nom,$_prenom,$_dateDeNaissance){
 
-                        $this->_nom=$nom;
+                        $this->_nom=$_nom;
                         $this->_prenom=$_prenom;
-                        $this->_date_de_naissance=$_date_de_naissance;
-    }   
+                        $this->_dateDeNaissance=new DateTime($_dateDeNaissance);
+                        
+    }  
+
+
+
     public function getinfo(){
 
-                       return $this->_nom;
-                       return $this->_prenom;
-                       return $this->_date_de_naissance;
+                       return $this->_nom . " " . $this->_prenom . " " . " née le " .  $this->_dateDeNaissance->format('Y-m-d');
+                       
                 }   
+    
+    public function getage(){
+
+                        $_age=$this->_dateDeNaissance->diff(new DateTime());
+                        echo " " . $_age->y. " ans ";
+    }
 
     }
-    $p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;
 
-    $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;
+    $p1 = new personne("DUPONT", "Michel", "1980-02-19") ;
+    $p2 = new personne("DUCHEMIN", "Alice", "1985-01-17") ;
+    
 
-    echo $p1->getinfo();
+    echo $p1->getinfo() . "," ;
+    echo " " . $p1->getage();
+    echo "<pre>";
+    echo $p2->getinfo() . "," ;
+    echo " " . $p2->getage() ;
+    
 
 ?>
