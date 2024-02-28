5f73a634e0880d2852946fe3b7dd2abc33044a22
Nosdi67 <rshakhmur@gmail.com>
2024-02-28 11:11:52 +0100

    Exo 3 php feuille 2

---
+++ php_exo_18.php
@@ -0,0 +1,10 @@
+<h1>Exo 18</h1>
+
+<p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir
+dans un nouvel onglet (_blank).</p>
+
+<?php
+
+echo '<a href=https://elan-formation.fr/accueil> Elan formatipn </a>'
+
+?>
